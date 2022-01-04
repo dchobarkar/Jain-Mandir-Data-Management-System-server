b8dbfc1190dbc580b51fade0d66a9dbab8ef36c7
Darshan Chobarkar <dchobarkar@gmail.com>
2022-01-04 12:32:57 +0530

    server configuration completed

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import * as config from 'config';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Get env values
+  const SERVER_CONFIG = config.get('server');
+
   const app = await NestFactory.create(AppModule);
 
   // Enable requests from other origins (To be disabled in static files serving )
   app.enableCors();
 
-  await app.listen(3000);
+  await app.listen(SERVER_CONFIG.port, '0.0.0.0');
 }
 bootstrap();
